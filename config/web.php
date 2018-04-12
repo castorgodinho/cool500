f2c49fe532f435446e10879667a5d8d71fbf666a
Castor Godinho <castorgodinho@yahoo.in>
2018-04-12 16:07:49 +0530

    RBAC added. Database changed

--- config/web.php
+++ config/web.php
@@ -8,6 +8,11 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            // uncomment if you want to cache RBAC items hierarchy
+            // 'cache' => 'cache',
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'vallanka',
