3ac7d3f7ca6ddc0746a519741fcba50003283a81
Castor Godinho <castorgodinho@yahoo.in>
2018-04-14 12:58:58 +0530

    commit before pull. Invoice dinw

--- views/orders/update.php
+++ views/orders/update.php
@@ -109,10 +109,25 @@ use yii\widgets\ActiveForm;
         <td>  Final Total ( C = A + B) </td>
         <td> <?= $currentDueTotal + $previousDueTotal ?>  </td>
       </tr>
-      
-
+    
+    
     </table>
     </div>
   </div>
 </div>
 
+<?php $form = ActiveForm::begin(); ?>
+
+<tr>
+  <td> <input value="<?= $order_id ?>" id="invoice-order_id" class="form-control" name="Invoice[order_id]" type="text"> </td>
+  <td> <input value="<?= $rate->rate_id ?>" id="invoice-rate_id" class="form-control" name="Invoice[rate_id]" type="text"> </td>
+  <td> <input value="<?= $tax->tax_id ?>" id="invoice-tax_id" class="form-control" name="Invoice[tax_id]" type="text"> </td>
+  <td> <input value="<?= $start_date ?>" id="invoice-start_date" class="form-control" name="Invoice[start_date]" type="text"> </td>
+  <td> <input value="<?= $interest->interest_id ?>" id="invoice-interest_id" class="form-control" name="Invoice[interest_id]" type="text"> </td>
+  <td> <input value="<?= $currentDueTotal + $previousDueTotal ?>" id="invoice-total_amount" class="form-control" name="Invoice[total_amount]" type="text"> </td>
+</tr>
+
+<div class="form-group">
+    <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+</div>
+  <?php ActiveForm::end(); ?>
